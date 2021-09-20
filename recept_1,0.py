dd31d4ba4f4bd4697199d4f4516ffee196e0e944
Levi-gal <90601416+Levi-gal@users.noreply.github.com>
2021-09-20 10:25:36 +0200

    Add files via upload

---
+++ recept_1,0.py
@@ -0,0 +1,10 @@
+def rakott_krumpli(cheese_type, kolbasz_type, drink):
+    print('Főzzük meg héjában a krumplit és a tojásokat is. Mindkét alapanyag ha megfőtt, pucoljuk meg, és vágjuk őket szeletekre.')
+    print('Míg a krumpli és tojás fő, addig vágjuk fel a ' + kolbasz_type + ' is karikákra.')
+    print('A zsírt olvasszuk meg, keverjük bele a tejfölt, és picit sózzuk meg, majd egy nagy tepsi alját ezzel kenjük ki. Karikázzuk fel a tojásokat és a krumplit.')
+    print('Első rétegnek a burgonya jöjjön, amit ha lepakoltunk, locsoljunk meg a zsíros tejföllel. Erre jöhet a tojás, amit ugyancsak zárásként locsoljunk meg. Majd a kolbászokat is rakjuk le egy rétegben, ugyan így a maradék burgonyát. Zárás képpen meg szorjuk meg ' + cheese_type + '.')
+    print('A maradék tejföllel locsoljuk meg az egészet, nem baj, ha kicsit "tocsog" a tejfölben a rakott krumpli, másnap annál jobb lesz.')
+    print('A tepsit tegyük 200 fokra előmelegített sütőbe, és 1 órán át süssük a krumplit. Ha kész, vegyük ki, egy picit pihentessük, aztán már fogyaszthatjuk is!')
+    print('Miután mindez kész, ' + drink + '. tálald')
+
+rakott_krumpli('Edami,füstölt_erős, sör')
